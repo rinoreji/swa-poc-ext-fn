223160ba12b61b97a47da500ce21c7ed59f4b2c6
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2022-09-20 16:57:13 +0530

    return roles

--- GetAppRoles.cs
+++ GetAppRoles.cs
@@ -19,17 +19,8 @@ namespace swa_poc_ext_fn
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            string[] roles = { "admin","custom-role1" };
+            return new JsonResult(new { roles });
         }
     }
 }
